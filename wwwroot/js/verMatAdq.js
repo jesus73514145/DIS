8eb74794631ed20d47a6939e78d104bebeab489f
yasseram <yasseravalosmontero14@gmail.com>
2024-10-19 20:02:57 -0500

    feat: abrir y cerrar modal de exportar agregado

--- wwwroot/js/verMatAdq.js
+++ wwwroot/js/verMatAdq.js
@@ -1,1 +1,15 @@
-alert("Hola")
+const modals = document.querySelectorAll('.modalExportar');
+const showButtons = document.querySelectorAll('.showModalExportar');
+const closeButtons = document.querySelectorAll('.closeModalExportar');
+
+showButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        modals[index].showModal(); // Abre el modal correspondiente al botón
+    });
+});
+
+closeButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        modals[index].close(); // Cierra el modal correspondiente
+    });
+});
